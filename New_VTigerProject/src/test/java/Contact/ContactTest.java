536d5a0ce8b7b7a07772bf93c69cfa43f67aef3f
SumitDEY01 <sumitdeyhd5@gmail.com>
2024-08-19 10:31:23 +0530

    new commmit

--- New_VTigerProject/src/test/java/Contact/ContactTest.java
+++ New_VTigerProject/src/test/java/Contact/ContactTest.java
@@ -3,26 +3,27 @@ package Contact;
 import org.testng.annotations.Test;
 
 public class ContactTest {
-		public void  ContactTest() {
+	@Test
+	public void  ContactTest() {
 
-		String URL=System.getProperty("url");
-		String BROWSER=System.getProperty("browser","chrome");
-		String USERNAME=System.getProperty("username");
-		String PASSWORD=System.getProperty("password");
-		
-		System.out.println("URL");
-		System.out.println("BROWSER");
-		System.out.println("USERNAME");
-		System.out.println("PASSWORD");
-		System.out.println("exeute createcontactTest");
+	String URL=System.getProperty("url");
+	String BROWSER=System.getProperty("browser","chrome");
+	String USERNAME=System.getProperty("username");
+	String PASSWORD=System.getProperty("password");
+	
+	System.out.println("URL");
+	System.out.println("BROWSER");
+	System.out.println("USERNAME");
+	System.out.println("PASSWORD");
+	System.out.println("exeute createcontactTest");
 
+}
+	
+	@Test
+	public void modifyContactTest() {
+
+		System.out.println("execute Modify Test");
 	}
-		
-		@Test
-		public void modifyContactTest() {
-
-			System.out.println("execute Modify Test");
-		}
-		
-		
+	
+	
 }
---
+++ New_VTigerProject/src/test/java/Organisation/AbcTest.java
@@ -0,0 +1,16 @@
+package Organisation;
+
+import org.testng.annotations.Test;
+
+public class AbcTest {
+	@Test
+	public void createORG() {
+		
+		System.out.println("Organisation Created");
+	}
+	@Test
+	public void modifyORG() {
+	System.out.println("Organisation Modifiedd");
+	}
+	
+}
