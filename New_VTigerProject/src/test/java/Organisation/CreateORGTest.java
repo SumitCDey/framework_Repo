4446366611a410f7340ad1e6276346b08ac510f3
SumitDEY01 <sumitdeyhd5@gmail.com>
2024-08-19 09:54:25 +0530

    m

---
+++ New_VTigerProject/src/test/java/Organisation/CreateORGTest.java
@@ -0,0 +1,16 @@
+package Organisation;
+
+import org.testng.annotations.Test;
+
+public class CreateORGTest {
+	@Test
+	public void createORG() {
+		
+		System.out.println("Organisation Created");
+	}
+	@Test
+	public void modifyORG() {
+	System.out.println("Organisation Modifiedd");
+	}
+
+}
